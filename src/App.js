0c698eda1c17d68d7dadba0e8cae4ad4c8aea346
Andres Crosa <andresmcrosa@gmail.com>
2022-12-17 21:39:31 -0300

    Estilo boton

--- src/App.js
+++ src/App.js
@@ -1,56 +1,77 @@
 import { useState } from 'react';
 import './App.css';
 
+
+const initialState = { name: '', price: 0 }
 const App = () => {
-  const [productName, setProductName] = useState('')
-  const [productPrice, setProductPrice] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const [product, setProduct] = useState(initialState)
+
+  const handleChange = (e) => {
+    const fieldName = e.target.name
+    const fieldValue = e.target.value
+    
+    setProduct({...product, [fieldName]: fieldValue })
+  }
   
-  const handleNameChange = (e) => {
-    const name = e.target.value
-    setProductName(name)
-  }
-
-  const handlePriceChange = (e) => {
-    const price = e.target.value
-    setProductPrice(price)
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault()
+    if(!product.name){
+      console.log('Tienes que llenar el campo nombre')
+      return
+    }
+
+    setIsLoading(true)
+
     fetch('http://localhost:8080/api/v1/products', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name: productName,
-        price: productPrice,
-      }),
+      body: JSON.stringify(product),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data.ok) {console.log("Producto creado con exito")
+      setProduct(initialState)
+      
+      } else {
+        console.log(data.message)
+      }
+      setIsLoading(false)
+    })
+    .catch((err) => {
+      console.log(err)
+      setIsLoading(false)
     })
   }
 
 
-console.log({productName})
-console.log({productPrice})
+console.log({product})
+
   return (
     <div className="App">
         <h1>Nuevo Producto</h1>
         <form onSubmit={handleSubmit}>
           <input 
-            onChange={handleNameChange}
-            value={productName} 
+            onChange={handleChange}
+            value={product.name} 
             type='text' 
             name='name' 
             placeholder='Nombre del Producto...'
           />
           <input 
-            onChange={handlePriceChange}
-            value={productPrice} 
+            onChange={handleChange}
+            value={product.price} 
             type='number' 
             name='price'
             placeholder='Precio del Producto...'
           />
-          <button>Crear Producto</button>
+          <button>
+            {
+              isLoading ? 'Creando Producto...' : 'Crear Producto'
+            }
+            </button>
         </form>
     </div>
   );
